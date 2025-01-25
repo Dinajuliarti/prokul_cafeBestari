3e0affa0cae2a79679ea0e65bab380baa9c1696f
Dinajuliarti <dina_juliarti@teknokrat.ac.id>
2025-01-25 22:24:22 +0700

    Dinajuliarti :Update Dokumentasi

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,7 @@ export default function Home() {
       {/* Navbar */}
       <header className="bg-[#a05a2c] text-white p-4 flex justify-between items-center">
         <h1 className="text-3xl font-bold">Bestarii</h1>
-        <nav>
-          <a href="#" className="text-lg">Produk</a>
-        </nav>
+
       </header>
 
       {/* Profil Owner */}
